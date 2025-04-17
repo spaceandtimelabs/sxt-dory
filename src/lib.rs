a27826d5a517c498d3add8de0a8d5774f17e568d
Jay White <JayWhite2357@users.noreply.github.com>
2025-04-17 08:59:21 -0400

    feat: add message structs
    There are several prover/verifier messages. We add structs defining these messages.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! # Dory
+//!
+//! Dory is a polynomial commitment scheme with excellent asymptotic performance as well as
+//! practical efficiency. It is based on the work of Jonathan Lee (<https://eprint.iacr.org/2020/1274.pdf>).
+//!
+//! This crate provides a Rust implementation of the commitment scheme, intended to be usable as a
+//! building block for other zk/SNARK protocols.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Module containing structs of the messages exchanged between the prover and verifier.
+///
+/// This consists of two messages each during the Dory-Reduce phase,
+/// the verifier challenge during the Fold-Scalars phase,
+/// and the final prover message during the Scalar-Product phase.
+pub mod messages;
